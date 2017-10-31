5ad9424568fd1b0d9bf7d674f73fc4d8f27bf08a
Pavlos <msf7415012@teilar.gr>
2017-10-31 12:00:46 +0200

    docs: Read File Funtioncs

--- src/read_file.h
+++ src/read_file.h
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Read the file with data and store them to Two-Dimensional array 
+ */
 void readFile(const char *fileName, double **array);
+
+/**
+ * Read the file with data and count the Rows and Columns 
+ * to allocate memory
+ */
 void countRowsCols(const char *fileName, int *rows, int *cols);
 #endif
