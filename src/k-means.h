5fe02d5f34cb83d729348f73d259dd659394b527
Pavlos <msf7415012@teilar.gr>
2017-11-12 20:14:21 +0200

    feat: randomize centers

--- src/k-means.h
+++ src/k-means.h
@@ -3,32 +3,64 @@
 //#include <stdio.h>
 //void fillData(int **array,  int nRow, int nCols);
 void initializeClusters(int *clusters, int *rows);
+void randomizeCenters(double *centers, int *k);
 
-int * run(double **pointers, int *rows, int dementions, int k)
+int * run(double **pointers, int *rows, int dimensions, int *k)
 {
+    // one dimensional array to store the cluster of each data row
     int *clusters;
-    double **centros;
+
+    // centers of k-means
+    double *centers;
 
     // allocate memory for clusters
     clusters = malloc((*rows) * sizeof(int));
     initializeClusters(clusters, rows);
 
-    // todo: init ceentros
-    // todo: allocate memory and pointers for centros
-    // todo: randomize centros
+    // allocate memory for centers
+    centers = malloc((*k) * sizeof(double));
+    randomizeCenters(centers, k);
 
     // todo: Do Kmeans algortithms
     // todo: function for evlikia apostasi
     // todo: average per cluster to calclulate new centros
+    
+    // K-means steps
+    // While you have change in clusters continue
+    // calculate for each point Euclidean distance from centers
+    // and add them to nears cluster (min distance from center)
+    // if point change cluster update the flag to continue run
+    // When you add all points to relevant cluster then
+    // calculate the average per cluster and set new centers
+    // re run all steps until ther are no change to cluster
+
 
     return clusters;
 }
 
+/**
+ * Set all clusters to zero value for first run
+ */
 void initializeClusters(int *clusters, int *rows)
 {
     int i;
-    for(i =0; i < *rows; i++){
+    for(i = 0; i < *rows; i++){
+        // todo: fix it to zero after update
         clusters[i] = 1;
+    }
+}
+
+/**
+ * Randomize Centers of K-means for first run
+ */
+void randomizeCenters(double *centers, int *k)
+{
+    int i;
+    for(i = 0; i < *k; i++)
+    {
+        // todo: fix random 
+        centers[i] = ((double)rand()/(double)RAND_MAX);;
+        printf("%f\n", centers[i]);
     }
 }
 
